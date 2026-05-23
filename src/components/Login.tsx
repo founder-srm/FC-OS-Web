895babeaa7eaa3fd3f97f928d6973aec9473999b
Om Pratap Dhaker <opdhaker2007@gmail.com>
2026-05-23 22:06:08 +0530

    Remove Supabase

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { type FormEvent, useState } from "react";
+
 import { Button } from "./ui/button";
 import {
   Card,
-  CardHeader,
-  CardTitle,
   CardContent,
   CardFooter,
+  CardHeader,
+  CardTitle,
 } from "./ui/card";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
@@ -18,8 +26,8 @@ const Login = () => {
           Log In
         </CardTitle>
       </CardHeader>
-      <CardContent className="mt-1">
-        <form>
+      <form>
+        <CardContent className="my-4">
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
@@ -28,6 +36,9 @@ const Login = () => {
                 type="email"
                 placeholder="name@mail.com"
                 required
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                className="font-sans"
               />
             </div>
             <div className="grid gap-2">
@@ -40,19 +51,23 @@ const Login = () => {
                   Forgot your password?
                 </Link>
               </div>
-              <Input id="password" type="password" required />
+              <Input
+                id="password"
+                type="password"
+                required
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                className="font-sans"
+              />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 };
