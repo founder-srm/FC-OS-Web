f85aff6dfbf273c5048e6bd916bd8d9a17658a5b
Om Pratap Dhaker <opdhaker2007@gmail.com>
2026-06-24 07:34:49 +0530

    Insanely over engineered copy button

---
+++ src/components/copy-button.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Check, Copy, Loader } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
+
+type CopyButtonProps = {
+  variant?:
+    | "default"
+    | "destructive"
+    | "ghost"
+    | "link"
+    | "outline"
+    | "secondary"
+    | null
+    | undefined;
+  size?: "default" | "lg" | "sm" | "xs";
+  text?: boolean;
+  value: string;
+};
+
+const CopyButton = ({
+  variant = "outline",
+  size = "xs",
+  text = false,
+  value,
+}: CopyButtonProps) => {
+  const [copyState, setCopyState] = useState<"idle" | "loading" | "success">(
+    "idle",
+  );
+
+  const handleCopy = async (text: string) => {
+    try {
+      setCopyState("loading");
+      await navigator.clipboard.writeText(text);
+      setTimeout(() => {
+        setCopyState("success");
+      }, 500);
+      setTimeout(() => {
+        setCopyState("idle");
+      }, 2000);
+    } catch {
+      setCopyState("idle");
+      toast.error("Failed to copy");
+    }
+  };
+
+  return (
+    <Button
+      variant={variant}
+      size={text ? size : size === "default" ? "icon" : `icon-${size}`}
+      onClick={() => handleCopy(value)}
+    >
+      { copyState === "idle" && <Copy /> }
+      { copyState === "loading" && <Loader className="animate-spin" /> }
+      { copyState === "success" && <Check /> }
+      { text && copyState === "idle" && "Copy" }
+      { text && copyState === "loading" && null }
+      { text && copyState === "success" && "Copied!" }
+    </Button>
+  );
+};
+export default CopyButton;
